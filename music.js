ffd21d8d00ab4991d73aa60f9f088bee39f02d53
SKC31 <sikeletekay@gmail.com>
2026-01-31 22:49:20 +0200

    Update music.js

--- music.js
+++ music.js
@@ -1,20 +1,12 @@
 const music = document.getElementById("bgMusic");
 const btn = document.getElementById("musicBtn");
 
-// try autoplay on page load
-window.addEventListener("load", () => {
-  music.play().catch(() => {
-    console.log("Autoplay blocked by browser");
-  });
-});
-
-// mute/unmute button
 btn.addEventListener("click", () => {
-  if (music.muted) {
-    music.muted = false;
-    btn.textContent = "🔇";
-  } else {
-    music.muted = true;
-    btn.textContent = "🎧";
-  }
+    if (music.paused) {
+        music.play();
+        btn.textContent = "🔇"; // changes button after pressed
+    } else {
+        music.pause();
+        btn.textContent = "PRESS ME KIYOMI"; // revert text if paused
+    }
 });
